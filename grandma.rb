63d92438f50a9b47a23a2f2925581758f894f164
Cuong Phan <at120906@actvn.edu.vn>
2020-01-07 11:33:06 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   # else
-  elsif phphrase.upcase
+elsif !!phrase.upcase
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
